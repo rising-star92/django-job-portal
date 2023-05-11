75f4247d4ccf7cfd5e2788082d5da0c11390f115
rising-star92 <ryan.service.1001@gmail.com>
2023-05-11 04:40:46 -0400

    add weather app

--- jobportal/urls.py
+++ jobportal/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('portal.urls'))
+    path('', include('portal.urls')),
+    path('weather/', include('weather.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
